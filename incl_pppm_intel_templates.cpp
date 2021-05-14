c8eece51b95b4b5166b9c59f67f4f1471fb100f0
srtee <uqstee4@uq.edu.au>
2021-05-14 11:53:18 +1000

    tester CMake compatibility

---
+++ incl_pppm_intel_templates.cpp
@@ -0,0 +1,14 @@
+// File "pppm_impl.cpp"
+#include "pppm_intel.cpp"
+
+using namespace LAMMPS_NS;
+
+template void PPPMIntel::particle_map<float,double>(IntelBuffers<float,double> *buffers); 
+template void PPPMIntel::particle_map<double,double>(IntelBuffers<double,double> *buffers);
+template void PPPMIntel::particle_map<float,float>(IntelBuffers<float,float> *buffers);
+template void PPPMIntel::make_rho<float,double,0>(IntelBuffers<float,double> *buffers); 
+template void PPPMIntel::make_rho<double,double,0>(IntelBuffers<double,double> *buffers);
+template void PPPMIntel::make_rho<float,float,0>(IntelBuffers<float,float> *buffers);
+template void PPPMIntel::make_rho<float,double,1>(IntelBuffers<float,double> *buffers); 
+template void PPPMIntel::make_rho<double,double,1>(IntelBuffers<double,double> *buffers);
+template void PPPMIntel::make_rho<float,float,1>(IntelBuffers<float,float> *buffers);
--- pppm_impl.cpp
+++
@@ -1,6 +0,0 @@
-// File "pppm_impl.cpp"
-#include "pppm_intel.cpp"
-
-template void PPPMIntel::particle_map<float,double>();  
-template void PPPMIntel::particle_map<double,double>();  
-template void PPPMIntel::particle_map<float,float>();  
